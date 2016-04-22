fd4c141e52b6bc5beaa5a21ff0935cfac8527eef
kth919 <kth9192@daum.net>
2016-04-22 23:07:16 +0900

    additional exam-14

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -15,23 +15,48 @@ public  class UserDao {
 
     }
 
-    public User get(Long id) throws ClassNotFoundException, SQLException {
+    public User get(final Long id) throws ClassNotFoundException, SQLException {
 
         StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
-        return jdbcContext.jdbcContextWithStatementStrategyForQuery(statementStrategy);
+        return jdbcContext.jdbcContextWithStatementStrategyForQuery(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+                preparedStatement.setLong(1, id);
+                return preparedStatement;
+            }
+        });
     }
 
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new AddUserStatementStrategy(user);
-        jdbcContext.jdbcContextStatementStrategyForUpdate(statementStrategy);
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        jdbcContext.jdbcContextStatementStrategyForUpdate(new StatementStrategy() {
 
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
 
+                preparedStatement = connection.prepareStatement("insert into userinfo (id, name, password) values(?, ? , ?)");
+                preparedStatement.setLong(1, user.getId());
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setString(3, user.getPassword());
+                return preparedStatement;
+            }
+        });
     }
 
-    public void delete(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
-        jdbcContext.jdbcContextStatementStrategyForUpdate(statementStrategy);
+    public void delete(final Long id) throws SQLException {
+        jdbcContext.jdbcContextStatementStrategyForUpdate(new StatementStrategy() {
+
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("delete from userinfo where id = ? ");
+                preparedStatement.setLong(1, id);
+                return preparedStatement;
+            }
+        });
     }
 
 
